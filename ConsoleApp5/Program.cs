604f0dd1a0a354315a24e9fc5fce904b231e8f3b
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-14 00:55:00 +0400

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿string Login(string username, string password)
+{
+    if(username == "asdasd" && password == "asdasd123")
+    {
+        return "logic succesfull";
+    }
+    else 
+    {
+
+        return "username or password is wrong";
+    
+    }
+}
+Console.WriteLine("username");
+string username = Console.ReadLine();
+Console.WriteLine("password");
+string password = Console.ReadLine();
+string result = Login(username, password);
+Console.WriteLine(result);
